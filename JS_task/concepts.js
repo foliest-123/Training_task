db229478cc974f75f3f9278ec9a89686a9a828a1
foilest-123 <thegreatvijay1991@gmail.com>
2024-01-08 12:27:58 +0530

    string method completed and arrays started

--- JS_task/concepts.js
+++ JS_task/concepts.js
@@ -1,34 +1,35 @@
 console.log("hello")
 
-function number_methods () {
+function basic_concepts () {
  
     //simple addition with two numbers
         let number1 = 10
         let number2 = 20
         let result = number1 + number2
         console.log(result)
+
   // number methods
         console.log(Number.isNaN(number1))
         console.log(Number.isInteger(number1))
-        console.log(Number.parseFloat(3.926).toFixed(2));
+        console.log(Number.parseFloat(3.926).toFixed(2))
+
   // changing datatypes
         console.log(number1.toString() + number2.toString())
         console.log(parseFloat("342.43") + parseFloat("342.43"))
-        
+
+
+  // string methods
+        var name = "vijay"
+        greeting = "Welcome"
+        console.log(greeting.concat(" " ,name))
+        console.log(name.indexOf('a'))
+        console.log(name.substring(2, name.length))
+        console.log(name.endsWith('a'))
+
 }
 
- function string_methods (){
-    var name = "vijay"
-    console.log(name.charAt(1))
-    
-
- }
 
 
 
 
-
-
-
-number_methods()
-string_methods()
+basic_concepts()
